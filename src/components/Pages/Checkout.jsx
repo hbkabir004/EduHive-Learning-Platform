34e7e44f89f53c0382d22eca92d1cb99c2be6250
ABIR <hbkabir004@gmail.com>
2022-10-28 00:00:10 +0600

    checkout added

--- src/components/Pages/Checkout.jsx
+++ src/components/Pages/Checkout.jsx
@@ -3,13 +3,14 @@ import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
     const selectedCourse = useLoaderData();
-    const { id, title, categoryName, price, about } = selectedCourse;
+    const { id, title, categoryName, price, about, img } = selectedCourse;
 
     return (
         <div className='flex justify-center align-middle px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 my-40'>
             <div class="p-20 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <svg class="mb-2 w-10 h-10 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+                <img class="rounded-t-lg" src={img} alt="" />
                 <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                <h6 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Category: {categoryName}</h6>
                 <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">{about}</p>
                 <h6 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Price: ${price}</h6>
 
