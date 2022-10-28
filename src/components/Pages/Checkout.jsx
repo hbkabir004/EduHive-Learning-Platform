f09e7308cbaf1a8ca197aacaf5258baef6df02f6
ABIR <hbkabir004@gmail.com>
2022-10-28 14:48:50 +0600

    Final Commit

--- src/components/Pages/Checkout.jsx
+++ src/components/Pages/Checkout.jsx
@@ -11,7 +11,7 @@ const Checkout = () => {
     return (
         <div className='dark:bg-gray-600'>
             <div className='flex justify-center align-middle px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 my-40'>
-                <div ref={ref} className="p-20 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div ref={ref} className="p-10 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <img className="rounded-t-lg" src={img} alt="" />
                     <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                     <h6 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Category: {categoryName}</h6>
--- src/components/Pages/PostCheckout.jsx
+++ src/components/Pages/PostCheckout.jsx
@@ -15,7 +15,7 @@ const PostCheckout = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </div>
-                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white ml-5 text-sm">Just Checked Out</h5>
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white ml-5">Just Checked Out</h5>
                     </div>
                     <div class="text-sm font-medium text-blue-600 hover:text-green-500 dark:hover:text-green-500 hover:font-semibold dark:text-blue-500">
                         Downloaded
@@ -26,7 +26,6 @@ const PostCheckout = () => {
                     <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
                         <li class="py-3 sm:py-4">
                             <div class="flex items-center space-x-4">
-                                {/* <div className='sm:flex'> */}
                                 <div class="flex-shrink-0 ">
                                     <img class="lg:w-8 lg:h-8 w-1/2 rounded-full" src={img} />
                                     <p class="lg:hidden block my-2 text-sm font-semibold ml-2 text-gray-900 truncate dark:text-white">
@@ -49,9 +48,8 @@ const PostCheckout = () => {
                                     <p class="hidden lg:block text-sm text-gray-500 truncate dark:text-gray-400">
                                         {about}
                                     </p>
-
                                 </div>
-                                {/* </div> */}
+
                                 <div class="lg:inline-flex items-center lg:text-base font-semibold text-gray-900 dark:text-white text-xl">
                                     Price: ${price}
                                 </div>
